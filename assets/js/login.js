7c4e55bc4199591dfaedae0e62b6c4a521a85ae1
zhangrui <1301968248@qq.com>
2020-10-04 23:04:33 +0800

    完成登录和注册的开发

---
+++ assets/js/login.js
@@ -0,0 +1,63 @@
+$(function () {
+  // 点击去注册
+  $("#link_reg").on("click", function () {
+    $(".login_box").hide();
+    $(".reg_box").show();
+  });
+
+  $("#link_login").on("click", function () {
+    $(".login_box").show();
+    $(".reg_box").hide();
+  });
+
+  // 自定义校验规则 从 layui上获取form对象
+  var form = layui.form;
+  var layer = layui.layer;
+  // 通过 form.verify() 函数自定义校验规则
+  form.verify({
+    // 自定义了一个叫做 pwd 的校验规则
+    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    // 校验两次密码是否一致
+    repwd: function (value) {
+      // 形参拿到的是确认密码框中的内容  还需要拿到密码框中的内容 
+      var pwd = $(".reg_box [name=password]").val();
+      if (pwd !== value) {
+        return '两次密码不一致'
+      }
+    }
+  });
+
+  // 监听注册表单事件
+  $("#form_reg").on("submit", function (e) {
+    e.preventDefault();
+    var data = { username: $("#form_reg [name=username]").val(), password: $("#form_reg [name=password]").val() };
+    $.post('/api/reguser', data, function (res) {
+      if (res.status !== 0) {
+        return layer.msg(res.message);
+      };
+      layer.msg('注册成功，请登录');
+      $("#link_login").click();
+    });
+  });
+
+  // 监听登录表单提交事件
+  $("#form_login").submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      url: '/api/login',
+      method: 'POST',
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg('登录失败')
+        };
+        layer.msg('登录成功');
+        // 将登录成功得到的 token 保存到 localStorage 中
+        localStorage.setItem('token', res.token);
+        // 跳转后台主页
+        location.href = '/index.html';
+      }
+    });
+  });
+
+});
